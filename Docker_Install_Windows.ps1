4acee7e38ec08b211345dc41504c5a1e54c69336
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-04 23:25:19 +0000

    Showing how to run a container

--- Docker_Install_Windows.ps1
+++ Docker_Install_Windows.ps1
@@ -1,4 +1,4 @@
-# Below install Docker on Windows
+# Below install Docker on Windows - my website has more information and screenshots of the below in action: https://lostintheclouds.net/install-docker-on-windows/
 
 # Install Docker module
 Install-Module DockerMSftProvider -Force
@@ -9,5 +9,8 @@ Install-Package Docker -ProviderName DockerMsftProvider
 # reboot the machine.
 Shutdown -r -f -t 00
 
-# Now container features are available you need to installed them - needs another reboot
+# Now container features are available you need to installed them - if another reboot is required it will show it as yes or no
 (Install-WindowsFeature Containers).RestartNeeded
+
+# You can now use Docker (this will download the hello-world container and run it)
+docker run hello-world
